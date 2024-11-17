38b6dd13f8d7d8af241b9d6b92065dae82511c50
none <none@example.com>
2024-11-17 09:17:31 -0500

    init

---
+++ src/Patches.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using HarmonyLib;
+using MGSC;
+using UnityEngine;
+
+namespace QM_SlowerHunger
+{
+    [HarmonyPatch(typeof(StarvationEffect), nameof(StarvationEffect.ProcessActionPoint))]
+    internal class Patches
+    {
+        [HarmonyPrefix]
+        private static void saveInitialCurrentLevel(StarvationEffect __instance, out int __state)
+        {
+            __state = __instance.CurrentLevel;
+        }
+
+        [HarmonyPostfix]
+        private static void transformCurrentLevelToFloat(StarvationEffect __instance, int __state)
+        {
+            float value = Plugin.Config.HungerRateMultiplier;
+            int num = Mathf.RoundToInt((float)__state - (float)(__state - __instance.CurrentLevel) * value);
+
+            if (num >= __state)
+            {
+                num--;
+            }
+
+            __instance.CurrentLevel = num;
+        }
+    }
+}
